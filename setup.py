6cb98b3d4d4f892499d20d002499c6c453402604
JenaRachana <rachanajena36@gmail.com>
2026-02-14 11:46:58 +0530

    my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Medical-chatbot',
+    version='0.1.0',
+    author='Rachana Jena',
+    author_email='rachanajena36@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
